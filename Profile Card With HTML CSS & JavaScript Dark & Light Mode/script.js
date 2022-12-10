44021703cffc218a2d75954a88b51d0963616464
venukaushik <venukaushik31@gmail.com>
2022-12-11 00:16:41 +0530

    some more xd

---
+++ Profile Card With HTML CSS & JavaScript Dark & Light Mode/script.js
@@ -0,0 +1,9 @@
+let body = document.querySelector('body');
+let dark = document.querySelector('#dark');
+dark.onclick = function () {
+    if (dark.checked == true) {
+        body.classList.add('darkMode');
+    } else {
+        body.classList.remove('darkMode');
+    }
+}
